e52751c4a6be433f2165e2a04351c16442fb40cb
Stokish <samtokish@gmail.com>
2021-10-23 16:32:10 +0600

    course key issue fix

---
+++ dashboard/signals.py
@@ -0,0 +1,13 @@
+from random import sample
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Course
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        instance.key = instance.course_name + instance.author.username + str(instance.lesson_cost) + instance.subject.name
+        instance.key = ''.join(sample(instance.key, k=len(instance.key)))
+        instance.key += str(instance.pk)
