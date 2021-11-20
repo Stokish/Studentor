cbf003d4a7c4d167b838d8034b30ec80b72b0414
Marara <57908460+marara02@users.noreply.github.com>
2021-11-20 21:29:32 +0600

    [UPD] changed method name, sender

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -5,8 +5,8 @@ from django.dispatch import receiver
 from .models import Course
 
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+@receiver(post_save, sender=Course)
+def create_course(sender, instance, created, **kwargs):
     if created:
         instance.key = instance.course_name + instance.author.username + str(instance.lesson_cost) + instance.subject.name
         instance.key = ''.join(sample(instance.key, k=len(instance.key)))
